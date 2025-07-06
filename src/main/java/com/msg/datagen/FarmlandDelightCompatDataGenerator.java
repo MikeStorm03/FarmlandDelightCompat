190d7a1cc88e74b2ab4a1832876e5949b247dae3
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-07-07 04:12:21 +0700

    first commit

---
+++ src/main/java/com/msg/datagen/FarmlandDelightCompatDataGenerator.java
@@ -0,0 +1,13 @@
+package com.msg.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FarmlandDelightCompatDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(TagGenerator::new);
+	}
+
+}
---
+++ src/main/java/com/msg/mixin/CropBlockMixin.java
@@ -0,0 +1,21 @@
+package com.msg.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.msg.datagen.TagGenerator;
+
+@Mixin(CropBlock.class)
+public class CropBlockMixin {
+	@Inject(method = "mayPlaceOn(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z", at = @At("HEAD"), cancellable = true)
+    private void injected(BlockState state, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(state.is(TagGenerator.FARMLAND));
+    }
+}
